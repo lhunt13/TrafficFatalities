08ba298f871d66819287d02e6b82080ab01702aa
Lamar Hunt <lhunt118@gmail.com>
2016-10-10 13:18:11 -0400

    Cleaned data, Plotted each state

--- ReadinData.R
+++ ReadinData.R
@@ -5,15 +5,7 @@ library(dplyr)
 ## Read in accident and person data
 path <- "/Users/lamarhuntiii/Documents/Second Year Classes/Data Science/data"
 for(i in 2008:2015){
-  #accident file
-    if(i <= 1982 | i >= 1994){
-        assign(paste0("accident",i),read.dbf(paste0(path, "/fars",i,"/accident.dbf")))
-    }
-    else{
-        assign(paste0("accident",i),read.dbf(paste0(path, "/fars",i,"/acc",i,".dbf")))
-    }
-  
-  #person file
+   #person file
     if(i <= 1982 | i >= 1994){
         assign(paste0("person",i),read.dbf(paste0(path, "/fars",i,"/person.dbf")))
     }
@@ -21,79 +13,65 @@ for(i in 2008:2015){
         assign(paste0("person",i),read.dbf(paste0(path, "/fars",i,"/per",i,".dbf")))
     }
 }
-## read in the DRIMPAIR data
-for(i in 2010:2015){
-    assign(paste0("drimpair",i),read.dbf(paste0(path, "/fars",i,"/drimpair.dbf")))
-}
 
-
-## Full join the accident and person rbind.fill
-## PER_TYP = 1 indicates Driver
-for(i in 2008:2015){
-    text <- paste0("d",i," <- full_join(accident",i,", person",i,", by='ST_CASE')")
-    eval(parse(text=text))
-}
-
-## rbind the dataframes together
-accident <- data.frame()
-for(i in 2008:2015){
-    text <- paste0("accident <- rbind.fill(accident, accident",i,")")
-    eval(parse(text=text))
-}
-
+## concatenate all the person files, and add a year variable
 person <- data.frame()
 for(i in 2008:2015){
+    text0 <- paste0("person",i,"$YEAR <- rep(",i,", dim(person",i,")[1])")
+    eval(parse(text=text0))
+    
     text <- paste0("person <- rbind.fill(person, person",i,")")
     eval(parse(text=text))
 }
 
+## get only info about drivers
+drivers <- person %>% filter(PER_TYP==1)
 
+## get ALC_RES for years prior to 2015 to agree with 2015 values
+drivers$ALC_RES[drivers$YEAR < 2015] <- 10*drivers$ALC_RES[drivers$YEAR < 2015]
 
-## get numbers of drivers who are impaired with alcohol
-## want people with (PER_TYP == 1) & (DRINKING = ) & (ALC_RES = )
-## we can also look at DRUGRES1-3
+## get AGE for year 2008, and 2009 to match years after that
+drivers$AGE[drivers$YEAR == 2008] <- ifelse(99, 999, drivers$AGE[drivers$YEAR == 2008])
 
+## add a variable indicating whether the driver was drunk
+drivers <- dplyr::mutate(drivers, drunk=(DRINKING==1 | (ALC_RES >= 500 & ALC_RES <= 940)))
+
+## missing data
+#drunk
+drivers$drunk <- ifelse((drivers$ALC_RES>940 & drivers$DRINKING %in% c(8,9)),
+                        NA,drivers$drunk)
+
+#age
+drivers$AGE <- ifelse(drivers$AGE %in% c(998,999), 
+                      NA,drivers$AGE)
+
+#sex
+drivers$SEX <- ifelse(drivers$SEX %in% c(8,9),
+                      NA,drivers$SEX)
+
+## get proportion of drunk drivers per month, per year, per state for 96 months
 ## get raw numbers of drunk driving fatalities in colorado
-accident <- mutate(accident, drunk=(DRUNK_DR > 0))
+col.drunk <- drivers %>% group_by(STATE, YEAR, MONTH) %>% 
+    filter(STATE==8) %>% 
+    summarize(prop=mean(drunk, na.rm=T))
+plot(1:96,col.drunk$prop,ylim=c(0,1))
 
-col.drunk <- accident %>% group_by(STATE, YEAR, MONTH) %>% 
-             filter(STATE==8) %>% 
-             summarize(mean(drunk)) %>%
-             print(n=nrow(.))
-plot(1:96,col.drunk$`mean(drunk)`)
+us.drunk <- drivers %>% group_by(YEAR, MONTH) %>% 
+    summarize(prop=mean(drunk, na.rm=T))
+points(1:96,us.drunk$prop, col="red")
 
-us.drunk <- accident %>% group_by(YEAR, MONTH) %>% 
-  summarize(mean(drunk))
-plot(1:96,us.drunk$`mean(drunk)`)
-
-
-
-## tabulate counts by state each year
-library(dplyr)
-for(i in 1975:2015){
-  text <- paste0("table(","accident",i,"$MONTH)")
-  print(eval(parse(text=text)))
+for(i in unique(drivers$STATE)){
+    drunk.props <- drivers %>% 
+        group_by(STATE, YEAR, MONTH) %>% 
+        filter(STATE==i) %>% 
+        summarize(prop=mean(drunk, na.rm=T))
+    plot(1:dim(drunk.props)[1], 
+         drunk.props$prop, 
+         ylim=c(0,1), 
+         main=paste(i))
 }
 
-## change all the dates to 4 digits
-for(i in 1975:1997){
-  text <- paste0("accident",i,"$YEAR <- ifelse(accident",i,"$YEAR==99, NA, ",i,")")
-  eval(parse(text=text))
-}
 
-## clean up months
-accident1975$MONTH <- ifelse(accident1975$MONTH==99, NA, accident1975$MONTH)
-accident1977$MONTH <- ifelse(accident1977$MONTH==99, NA, accident1977$MONTH)
-
-
-## plotting no. fatalities by year for each state
-for(i in 1:56){
-  y <- numeric(0)
-  for(j in 1:41){
-    y[j] <- eval(parse(text= paste0("table(accident",j+1974,"$STATE)[",i,"]")))
-  }
-  plot(1975:2015, y, main=as.character(i), xlab="Year")
-}
 
 
 
