e6d20b6b759fa20beefa5346dfa85d062e087851
Lamar Hunt <lhunt118@gmail.com>
2016-10-19 14:57:56 -0400

    Added Variable Definitions

--- ReadinData.R
+++ ReadinData.R
@@ -2,8 +2,6 @@ library(foreign)
 library(plyr)
 library(dplyr)
 library(lubridate)
-library(changepoint)
-library(ggplot2)
 
 
 ## Read in person data
@@ -61,23 +59,25 @@ drivers$DAY <- ifelse(drivers$DAY==99,
 ##compute dates from year and month variables
 drivers$DATE <- ymd(paste(drivers$YEAR, drivers$MONTH, drivers$DAY, sep="-"))
 
-##logistic regression model with discontinuity at Jan 1st, 2015
-model1 <- glm(drunk ~ DATE + (DATE >= ymd("2015-1-1")) + DATE*(DATE >= ymd("2015-1-1")),
-    data=drivers[drivers$STATE==45,],
-    family="binomial")
-
 
 ## get proportion of drunk drivers per month, per year, per state for 96 months
 ## get raw numbers of drunk driving fatalities in colorado
+## let the day be the avg day for that month
 sc.drunk <- drivers %>% group_by(STATE, YEAR, MONTH) %>% 
     filter(STATE==45) %>%  
-    summarize(prop=mean(drunk, na.rm=T), DAY=round(mean(DAY))) #let the day be the avg day for that month
+    summarize(prop=mean(drunk, na.rm=T), DAY=round(mean(DAY))) 
 sc.drunk$DATE <- ymd(paste(sc.drunk$YEAR, sc.drunk$MONTH, sc.drunk$DAY, sep="-"))
 plot(sc.drunk$DATE, sc.drunk$prop, type="l")
+hist(log(sc.drunk$prop[sc.drunk$DATE < ymd("2015-1-1")]))
+hist(sc.drunk$prop[sc.drunk$DATE < ymd("2015-1-1")])
+
 model2 <- glm(log(prop) ~ DATE + (DATE >= ymd("2015-1-1")) + DATE*(DATE >= ymd("2015-1-1")), 
               data=sc.drunk)
-with(sc.drunk, plot(DATE, log(prop), type="l", ylim=c(-5,0)))
+with(sc.drunk, plot(DATE, log(prop), type="l"))
 lines(sc.drunk$DATE, model2$fitted.values, col="red")
+
+
+
 
 ## convert to ts object and plot
 southcarolina <- ts(sc.drunk$prop, frequency=12, start=c(2008,1))
