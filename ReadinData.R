ba98f9e316e39ec72e8100c2c70ef04da2c53d64
Lamar Hunt <lhunt118@gmail.com>
2016-10-19 15:40:29 -0400

    Updated Methods

--- ReadinData.R
+++ ReadinData.R
@@ -68,15 +68,18 @@ sc.drunk <- drivers %>% group_by(STATE, YEAR, MONTH) %>%
     summarize(prop=mean(drunk, na.rm=T), DAY=round(mean(DAY))) 
 sc.drunk$DATE <- ymd(paste(sc.drunk$YEAR, sc.drunk$MONTH, sc.drunk$DAY, sep="-"))
 plot(sc.drunk$DATE, sc.drunk$prop, type="l")
-hist(log(sc.drunk$prop[sc.drunk$DATE < ymd("2015-1-1")]))
-hist(sc.drunk$prop[sc.drunk$DATE < ymd("2015-1-1")])
+hist(log(sc.drunk$prop[sc.drunk$DATE < ymd("2014-10-1")]))
+hist(sc.drunk$prop[sc.drunk$DATE < ymd("2014-10-1")])
 
-model2 <- glm(log(prop) ~ DATE + (DATE >= ymd("2015-1-1")) + DATE*(DATE >= ymd("2015-1-1")), 
+model2 <- glm(log(prop) ~ DATE + (DATE >= ymd("2014-10-1")) + DATE*(DATE >= ymd("2014-10-1")), 
               data=sc.drunk)
 with(sc.drunk, plot(DATE, log(prop), type="l"))
 lines(sc.drunk$DATE, model2$fitted.values, col="red")
 
-
+model3 <- glm(log(prop) ~ DATE + (DATE >= ymd("2013-1-1")) + DATE*(DATE >= ymd("2013-1-1")), 
+              data=sc.drunk)
+with(sc.drunk, plot(DATE, log(prop), type="l"))
+lines(sc.drunk$DATE, model3$fitted.values, col="red")
 
 
 ## convert to ts object and plot
